9b12b8acad2fde278f6c329f2404b552e7fc621a
Jose del Río <joserio@bysidecar.com>
2019-05-31 12:00:32 +0200

    Main method. Initializes the Db environment and set the available routes for this API

--- pkg/api.go
+++ pkg/api.go
@@ -1,6 +1,9 @@
 package apic2c
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
@@ -8,7 +11,10 @@ import (
 // Handler is a struct created to use its ch property as element that implements
 // http.Handler.Neededed to call HandleFunction as param in router Handler function.
 type Handler struct {
-	ch http.Handler
+	ch       http.Handler
+	Storer   Storer
+	Lead     Lead
+	LeadTest LeadTest
 }
 
 // HandleFunction is a function used to manage all received requests.
@@ -18,13 +24,54 @@ type Handler struct {
 // Returns StatusInternalServerError when decoding the body content fails.
 func (ch *Handler) HandleFunction() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		if err := ch.Lead.decode(r.Body); err != nil {
+			message := fmt.Sprintf("Error decoding lead, Err: %v", err)
+			responseError(w, message, err)
+			return
+		}
+		responseOk(w, "OK")
+	})
+}
+
+// TestHandler blablabla
+func (ch *Handler) TestHandler(w http.ResponseWriter, req *http.Request) {
+	responseOk(w, "TestHandler")
+}
+
+// RcableHandler blablabla
+func (ch *Handler) RcableHandler(w http.ResponseWriter, req *http.Request) {
+
+	if err := ch.Lead.decode(req.Body); err != nil {
+		message := fmt.Sprintf("Error decoding lead, Err: %v", err)
+		responseError(w, message, err)
+		return
+	}
+
+	ch.Storer.Instance()
+
+	ch.Storer.Insert(&ch.Lead)
+
+	// leadLeontel := ch.Lead.LeadToLeontel()
+	// log.Println(leadLeontel)
+	responseOk(w, "OK")
+}
+
+// Middleware to preproccess requests. Checks for the correct content-type
+func Middleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
 			log.Println("Method not allowed ", http.StatusMethodNotAllowed)
 			http.Error(w, "Method not allowed ", http.StatusMethodNotAllowed)
 			return
 		}
-
-		w.Header().Set("Content-Type", "application/json")
-		// json.NewEncoder(w).Encode()
+		h.ServeHTTP(w, r)
 	})
+}
+
+func (lead *Lead) decode(body io.ReadCloser) error {
+	if err := json.NewDecoder(body).Decode(lead); err != nil {
+		return err
+	}
+	return nil
 }
