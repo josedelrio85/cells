299155e725a57bc81bc28535b229d9c0f678e028
Jose del Río <joserio@bysidecar.com>
2019-05-31 12:00:48 +0200

    Main file

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	apic2c "github.com/bysidecar/api_ws/pkg"
 	"github.com/rs/cors"
@@ -11,10 +13,67 @@ import (
 )
 
 func main() {
-	r := mux.NewRouter()
 
-	ch := apic2c.Handler{}
-	r.PathPrefix("/apic2c/").Handler(ch.HandleFunction())
+	port := GetSetting("DB_PORT")
+	portInt, err := strconv.ParseInt(port, 10, 64)
+	if err != nil {
+		log.Fatalf("Error parsing to string Database's port %s, Err: %s", port, err)
+	}
 
-	log.Fatal(http.ListenAndServe(":5000", cors.Default().Handler(r)))
+	database := &apic2c.Database{
+		Host:      GetSetting("DB_HOST"),
+		Port:      portInt,
+		User:      GetSetting("DB_USER"),
+		Pass:      GetSetting("DB_PASS"),
+		Dbname:    GetSetting("DB_NAME"),
+		Charset:   "utf8",
+		ParseTime: "True",
+		Loc:       "Local",
+	}
+	ch := apic2c.Handler{
+		Storer: database,
+	}
+
+	if err := database.Open(); err != nil {
+		log.Fatalf("error opening database connection. err: %s", err)
+	}
+	defer database.Close()
+
+	lead := apic2c.Lead{}
+	leadtest := apic2c.LeadTest{}
+
+	if err := database.CreateTable(lead); err != nil {
+		log.Fatalf("error creating the table. err: %s", err)
+	}
+
+	if err := database.CreateTable(leadtest); err != nil {
+		log.Fatalf("error creating the table. err: %s", err)
+	}
+
+	router := mux.NewRouter().StrictSlash(true)
+	base := router.PathPrefix("/apic2c").Subrouter()
+
+	rcable := base.PathPrefix("/rcable").Subrouter()
+	rcable.HandleFunc("/incomingC2C", ch.RcableHandler)
+
+	creditea := base.PathPrefix("/creditea").Subrouter()
+	creditea.HandleFunc("/test", ch.TestHandler)
+
+	router.Use(apic2c.Middleware)
+	log.Fatal(http.ListenAndServe(":5000", cors.Default().Handler(router)))
+}
+
+// GetSetting reads an ENV VAR setting, it does crash the service if with an
+// error message if any setting is not found.
+//
+// - setting: The setting (ENV VAR) to read.
+//
+// Returns the setting value.
+func GetSetting(setting string) string {
+	value, ok := os.LookupEnv(setting)
+	if !ok {
+		log.Fatalf("Init error, %s ENV var not found", setting)
+	}
+
+	return value
 }
