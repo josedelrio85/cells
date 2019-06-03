03c4617c1c64f36999b439d81265f23b5d451384
Jose del Río <joserio@bysidecar.com>
2019-06-03 13:36:44 +0200

    no changes

--- pkg/db.go
+++ pkg/db.go
@@ -87,7 +87,9 @@ func (env *Database) Update(element interface{}, wCond string, wFields []string)
 		wFieldsArr = append(wFieldsArr, z)
 	}
 	env.db.Debug().Model(&element).Where(wCond, true).Update(wFieldsArr...)
-
+	
+	// db.Model(&user).Where("active = ?", true).Update("name", "hello")
+	// UPDATE users SET name='hello', updated_at='2013-11-17 21:34:10' WHERE id=111 AND active=true;
 	return nil
 }
 
