5492d0c8158ef4e3ad0dc5c16d9ecc45b3db94ed
Jose del Río <joserio@bysidecar.com>
2019-07-16 09:56:10 +0200

    tests for asnef validations

--- pkg/hooks/asnef_test.go
+++ pkg/hooks/asnef_test.go
@@ -1,6 +1,7 @@
 package leads
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,7 +9,7 @@ import (
 	model "github.com/bysidecar/leads/pkg/model"
 )
 
-func ActiveTest(t *testing.T) {
+func TestActive(t *testing.T) {
 	assert := assert.New(t)
 
 	var asnef Asnef
@@ -21,9 +22,42 @@ func ActiveTest(t *testing.T) {
 		{
 			Description: "when Asnef successfully is activated",
 			Lead: model.Lead{
-				SouID: 1,
+				SouID:     9,
+				IsLeontel: true,
 			},
 			Active: true,
+		},
+		{
+			Description: "when Asnef successfully is activated",
+			Lead: model.Lead{
+				SouID:     58,
+				IsLeontel: true,
+			},
+			Active: true,
+		},
+		{
+			Description: "when Asnef is not activated",
+			Lead: model.Lead{
+				SouID:     99,
+				IsLeontel: true,
+			},
+			Active: false,
+		},
+		{
+			Description: "when Asnef is not activated",
+			Lead: model.Lead{
+				SouID:     0,
+				IsLeontel: true,
+			},
+			Active: false,
+		},
+		{
+			Description: "when isleontel is false",
+			Lead: model.Lead{
+				SouID:     9,
+				IsLeontel: false,
+			},
+			Active: false,
 		},
 	}
 
@@ -36,10 +70,19 @@ func ActiveTest(t *testing.T) {
 	}
 }
 
-func PerformTest(t *testing.T) {
+func TestPerform(t *testing.T) {
 	assert := assert.New(t)
 
 	var asnef Asnef
+	phone := "665932355"
+	dni := "79317432t"
+	motivo := "Check Asnef marcado."
+	cantidad := "1000"
+
+	lead := model.Lead{
+		SouID: 9,
+	}
+	candidates := GetCandidates(lead)
 
 	tests := []struct {
 		Description string
@@ -48,16 +91,66 @@ func PerformTest(t *testing.T) {
 	}{
 		{
 			Description: "when Asnef successfully checks and gives ok to the lead",
-			Lead:        model.Lead{},
-			Response:    HookResponse{},
+			Lead: model.Lead{
+				SouID:     9,
+				LeaPhone:  &phone,
+				LeaDNI:    &dni,
+				IsLeontel: true,
+				Creditea: model.Creditea{
+					Motivo: nil,
+				},
+			},
+			Response: HookResponse{
+				Err:        nil,
+				StatusCode: http.StatusOK,
+				Result:     false,
+			},
+		},
+		{
+			Description: "when Asnef/Already client checks were clicked. Client activates the limitation",
+			Lead: model.Lead{
+				SouID:     9,
+				LeaPhone:  &phone,
+				LeaDNI:    &dni,
+				IsLeontel: true,
+				Creditea: model.Creditea{
+					Cantidadsolicitada: &cantidad,
+					Motivo:             &motivo,
+				},
+			},
+			Response: HookResponse{
+				Err:        nil,
+				StatusCode: http.StatusOK,
+				Result:     true,
+			},
+		},
+		{
+			Description: "when Asnef validations is not passed",
+			Lead: model.Lead{
+				SouID:     9,
+				LeaPhone:  &candidates[0].Telefono,
+				LeaDNI:    &candidates[0].DNI,
+				IsLeontel: false,
+				Creditea: model.Creditea{
+					Motivo: nil,
+				},
+			},
+			Response: HookResponse{
+				Err:        nil,
+				StatusCode: http.StatusOK,
+				Result:     true,
+			},
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.Description, func(t *testing.T) {
-			response := asnef.Perform(test.Lead)
+			response := asnef.Perform(&test.Lead)
 
 			assert.Equal(test.Response, response)
+			assert.Equal(test.Response.Result, response.Result)
+			assert.Equal(test.Response.StatusCode, response.StatusCode)
+			assert.Equal(test.Response.Err, response.Err)
 		})
 	}
 }
