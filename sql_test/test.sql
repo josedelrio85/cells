c39ea90bc04ad6f038f8339e9eb935bf4fd5f897
Jose del Río <joserio@bysidecar.com>
2019-07-29 11:55:02 +0200

    Fixed test table name

--- sql_test/test.sql
+++ sql_test/test.sql
@@ -1,4 +1,4 @@
-CREATE TABLE leadnew (
+CREATE TABLE leads (
   id int(10) unsigned NOT NULL AUTO_INCREMENT,
   created_at timestamp NULL DEFAULT NULL,
   updated_at timestamp NULL DEFAULT NULL,
@@ -23,7 +23,7 @@ CREATE TABLE leadnew (
   domain varchar(255) COLLATE utf8_spanish_ci DEFAULT NULL,
   observations text COLLATE utf8_spanish_ci,
   PRIMARY KEY (id),
-  KEY idx_leadnew_deleted_at (deleted_at)
+  KEY idx_leads_deleted_at (deleted_at)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 
