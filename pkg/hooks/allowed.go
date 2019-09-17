0b4b62151225be2857b98a798e9a2edd8f2bb14a
Jose del Río <joserio@bysidecar.com>
2019-09-17 17:11:03 +0200

    Created allowed hook to create a barrier to those leads received in a period samller than 60 seconds

--- pkg/hooks/allowed.go
+++ pkg/hooks/allowed.go
@@ -1,29 +1,27 @@
 package leads
 
 import (
-	"crypto/rand"
 	"fmt"
-	"io"
 	"net/http"
 
+	container "github.com/bysidecar/leads/pkg/container"
 	model "github.com/bysidecar/leads/pkg/model"
-	"github.com/jinzhu/gorm"
+
+	"github.com/pkg/errors"
 )
 
-// Allowed is a struct
+// Allowed is a is a struct that represents a Redis entity
 type Allowed struct {
-	Result bool   `json:"result,omitempty"`
-	Error  string `json:"error,omitempty"`
-	Redis  model.Redis
+	Redis model.Redis
 }
 
 // Active implents the Hooable interface, so when checking
-// for active hooks will trigger the Asnef hook
+// for active hooks will trigger the hook
 // when the SouID matches a closed list.
 //
-// lead: The lead to check Asneff on.
+// lead: The lead to check on.
 //
-// Returns true if the Asnef Hook gets activated.
+// Returns true if the hook gets activated.
 func (a Allowed) Active(lead model.Lead) bool {
 	switch lead.SouID {
 	case 64:
@@ -38,14 +36,15 @@ func (a Allowed) Active(lead model.Lead) bool {
 }
 
 // Perform returns the result of allowed validation
-// lead: The lead to check Asnef on.
+// lead: The lead to check on.
 // db: not used in this implementation
 // Returns a HookReponse with the allowed check result.
-func (a Allowed) Perform(db *gorm.DB, lead *model.Lead) HookResponse {
-
+func (a Allowed) Perform(cont container.Container) HookResponse {
+	lead := cont.Lead
 	phone := *lead.LeaPhone
 	key := fmt.Sprintf("%s-%d-%d", phone, lead.SouID, lead.LeatypeID)
-	_, err := a.Redis.Get(key)
+
+	redisvalue, err := cont.Redis.Get(key)
 	if err != nil {
 		return HookResponse{
 			StatusCode: http.StatusUnprocessableEntity,
@@ -53,29 +52,21 @@ func (a Allowed) Perform(db *gorm.DB, lead *model.Lead) HookResponse {
 		}
 	}
 
-	a.Redis.Set(key, phone)
+	// if a value is returned from Get than its length > 0
+	// then there is a match in redis environment and we must reject the lead
+	if len(*redisvalue) > 0 {
+		message := fmt.Sprintf("Max attempts limit reached %s", key)
+		return HookResponse{
+			StatusCode: http.StatusUnprocessableEntity,
+			Err:        errors.New(message),
+		}
+	}
+
+	// if ther isn't a value we set it
+	cont.Redis.Set(key, phone)
 
 	return HookResponse{
 		StatusCode: http.StatusOK,
 		Err:        nil,
 	}
-}
-
-func generateCode() (*string, error) {
-	b := make([]byte, 8)
-	n, err := io.ReadAtLeast(rand.Reader, b, 8)
-	if n != 8 {
-		return nil, err
-	}
-	for i := 0; i < len(b); i++ {
-		b[i] = codeTable[int(b[i])%len(codeTable)]
-	}
-
-	code := string(b)
-	return &code, nil
-}
-
-var codeTable = [...]byte{
-	'1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-	'A', 'C', 'E', 'G', 'H', 'K', 'M', 'N', 'R', 'P', 'Q', 'R', 'S', 'X', 'Y', 'Z',
 }
