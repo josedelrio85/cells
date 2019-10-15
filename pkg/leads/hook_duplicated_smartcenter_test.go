34bacbf6a8394ccbf08027ba4ca16b26a82431b4
Jose del Río <joserio@bysidecar.com>
2019-10-15 11:24:26 +0200

    Checks for "duplicated" lead in Foobartel environment. Tests not finished.

---
+++ pkg/leads/hook_duplicated_smartcenter_test.go
@@ -0,0 +1,125 @@
+package leads
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestActiveDuplicatedSC(t *testing.T) {
+	assert := assert.New(t)
+
+	var duplicated DuplicatedSmartCenter
+
+	tests := []struct {
+		Description string
+		Lead        Lead
+		Active      bool
+	}{
+		{
+			Description: "when DuplicatedSmartCenter hook is successfully activated",
+			Lead: Lead{
+				SouID: 64,
+			},
+			Active: true,
+		},
+		{
+			Description: "when DuplicatedSmartCenter hook is successfully activated",
+			Lead: Lead{
+				SouID: 15,
+			},
+			Active: true,
+		},
+		{
+			Description: "when DuplicatedSmartCenter hook is not activated",
+			Lead: Lead{
+				SouID: 1,
+			},
+			Active: false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Description, func(t *testing.T) {
+			active := duplicated.Active(test.Lead)
+
+			assert.Equal(test.Active, active)
+		})
+	}
+}
+
+func TestPerformDuplicatedSC(t *testing.T) {
+	assert := assert.New(t)
+
+	var duplicated DuplicatedSmartCenter
+	phone1 := HelperRandstring(9)
+	phone2 := HelperRandstring(9)
+	phone3 := HelperRandstring(9)
+	// TODO insert a lead in Leontel to test and then delete/close it
+	// The 2 first cases only checks if the lead exists in Leontel, for the third case we must
+	// insert a lead into Leontel, assert that exists versus test data and close/delete it
+	tests := []struct {
+		Description    string
+		Lead           Lead
+		Response       HookResponse
+		ExpectedResult bool
+	}{
+		{
+			Description: "When a lead is not duplicated in smart center",
+			Lead: Lead{
+				LeaPhone:  &phone1,
+				SouID:     64,
+				LeatypeID: 1,
+			},
+			Response: HookResponse{
+				StatusCode: http.StatusOK,
+				Err:        nil,
+			},
+			ExpectedResult: true,
+		},
+		{
+			Description: "When another lead is not duplicated in smartcenter",
+			Lead: Lead{
+				LeaPhone:  &phone2,
+				SouID:     15,
+				LeatypeID: 24,
+			},
+			Response: HookResponse{
+				StatusCode: http.StatusOK,
+				Err:        nil,
+			},
+			ExpectedResult: true,
+		},
+		{
+			Description: "When a lead is duplicated because is open in Leontel environment",
+			Lead: Lead{
+				LeaPhone:  &phone3,
+				SouID:     64,
+				LeatypeID: 1,
+			},
+			Response: HookResponse{
+				StatusCode: http.StatusUnprocessableEntity,
+				Err:        errors.New("Error"),
+			},
+			ExpectedResult: false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Description, func(t *testing.T) {
+			cont := Handler{
+				Lead: test.Lead,
+			}
+			response := duplicated.Perform(&cont)
+
+			assert.Equal(test.Response.StatusCode, response.StatusCode)
+			if test.ExpectedResult {
+				assert.Nil(response.Err)
+			} else {
+				assert.NotNil(response.Err)
+			}
+		})
+	}
+}
