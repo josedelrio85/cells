b895befe9c0492355a9fb78dff1220b6019acdff
Jose del Río <joserio@bysidecar.com>
2019-10-15 17:44:45 +0200

    testv2

--- pkg/leads/hook_duplicated_time_test.go
+++ pkg/leads/hook_duplicated_time_test.go
@@ -163,7 +163,7 @@ func TestPerformDuplicated(t *testing.T) {
 			var response HookResponse
 			if test.Sleep {
 				// exptime := time.Duration(duplicated.getExpirationTime(test.Lead.SouID)) * time.Second
-				exptime := time.Duration(60) * time.Second
+				exptime := time.Duration((duplicated.getExpirationTime(test.Lead.SouID) + 1)) * time.Second
 				time.Sleep(exptime)
 				response = duplicated.Perform(&cont)
 			} else {
