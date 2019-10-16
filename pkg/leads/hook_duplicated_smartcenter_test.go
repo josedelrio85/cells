44bd3a3dea3c09fcfd8bc9ecca101441df8fbbd8
Jose del Río <joserio@bysidecar.com>
2019-10-16 17:46:19 +0200

    Redo tests for this hook using the approach of implement a mock request and its response and evaluate it against a previously supplied input

--- pkg/leads/hook_duplicated_smartcenter_test.go
+++ pkg/leads/hook_duplicated_smartcenter_test.go
@@ -1,10 +1,14 @@
 package leads
 
 import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
 	"testing"
+	"time"
 
-	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -53,76 +57,118 @@ func TestActiveDuplicatedSC(t *testing.T) {
 func TestPerformDuplicatedSC(t *testing.T) {
 	assert := assert.New(t)
 
-	var duplicated DuplicatedSmartCenter
 	phone1 := HelperRandstring(9)
 	phone2 := HelperRandstring(9)
 	phone3 := HelperRandstring(9)
-	// TODO insert a lead in Leontel to test and then delete/close it
-	// The 2 first cases only checks if the lead exists in Leontel, for the third case we must
-	// insert a lead into Leontel, assert that exists versus test data and close/delete it
+
 	tests := []struct {
-		Description    string
-		Lead           Lead
-		Response       HookResponse
-		ExpectedResult bool
+		Description      string
+		Lead             Lead
+		ExpectedStatus   int
+		ExpectedResponse RespSC
 	}{
 		{
 			Description: "When a lead is not duplicated in smart center",
 			Lead: Lead{
-				LeaPhone:  &phone1,
-				SouID:     64,
-				LeatypeID: 1,
+				LeaPhone:         &phone1,
+				SouIDLeontel:     73,
+				LeatypeIDLeontel: 2,
 			},
-			Response: HookResponse{
-				StatusCode: http.StatusOK,
-				Err:        nil,
+			ExpectedStatus: http.StatusOK,
+			ExpectedResponse: RespSC{
+				Success: false,
+				Data:    nil,
+				Error:   nil,
 			},
-			ExpectedResult: true,
 		},
 		{
 			Description: "When another lead is not duplicated in smartcenter",
 			Lead: Lead{
 				LeaPhone:  &phone2,
-				SouID:     15,
-				LeatypeID: 24,
+				SouID:     23,
+				LeatypeID: 26,
 			},
-			Response: HookResponse{
-				StatusCode: http.StatusOK,
-				Err:        nil,
+			ExpectedStatus: http.StatusOK,
+			ExpectedResponse: RespSC{
+				Success: false,
+				Data:    nil,
+				Error:   nil,
 			},
-			ExpectedResult: true,
 		},
 		{
 			Description: "When a lead is duplicated because is open in Leontel environment",
 			Lead: Lead{
 				LeaPhone:  &phone3,
-				SouID:     64,
-				LeatypeID: 1,
+				SouID:     73,
+				LeatypeID: 2,
 			},
-			Response: HookResponse{
-				StatusCode: http.StatusUnprocessableEntity,
-				Err:        errors.New("Error"),
+			ExpectedStatus: http.StatusOK,
+			ExpectedResponse: RespSC{
+				Success: true,
+				Data: []DataLeontelResp{
+					DataLeontelResp{
+						Closed:      "0",
+						LeaID:       "999999",
+						LeaTs:       time.Now().Format("2006-01-02 15:04:05"),
+						Description: "Test",
+						SubID:       "99",
+					},
+				},
+				Error: nil,
 			},
-			ExpectedResult: false,
 		},
 	}
 
-	for index, test := range tests {
+	for _, test := range tests {
 		t.Run(test.Description, func(t *testing.T) {
-			cont := Handler{
-				Lead: test.Lead,
-			}
-			response := duplicated.Perform(&cont)
 
-			// TODO third case does not pass the tests, remove this snippet when it is fixed
-			if index < 2 {
-				assert.Equal(test.Response.StatusCode, response.StatusCode)
-				if test.ExpectedResult {
-					assert.Nil(response.Err)
-				} else {
-					assert.NotNil(response.Err)
-				}
+			// generate a test server so we can capture and inspect the request
+			testServer := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+				testresp, err := json.Marshal(test.ExpectedResponse)
+				assert.NoError(err)
+
+				// Writes the status response that we expect for the actual test
+				res.WriteHeader(test.ExpectedStatus)
+
+				// Writes the response that we expect for the actual test
+				res.Write(testresp)
+			}))
+			defer func() { testServer.Close() }()
+
+			// Pass the leontel data for souid and type
+			data := struct {
+				Phone   string `json:"TELEFONO"`
+				SouID   int64  `json:"lea_source"`
+				LeaType int64  `json:"lea_type"`
+			}{
+				Phone:   *test.Lead.LeaPhone,
+				SouID:   test.Lead.SouIDLeontel,
+				LeaType: test.Lead.LeatypeIDLeontel,
 			}
+
+			bytevalues, err := json.Marshal(data)
+			assert.NoError(err)
+
+			req, err := http.NewRequest(http.MethodPost, testServer.URL, bytes.NewBuffer(bytevalues))
+			assert.NoError(err)
+
+			resp, err := http.DefaultClient.Do(req)
+			assert.NoError(err)
+
+			rawdata, _ := ioutil.ReadAll(resp.Body)
+			structdata := RespSC{}
+
+			err = json.Unmarshal(rawdata, &structdata)
+			assert.NoError(err)
+
+			assert.Equal(test.ExpectedResponse.Success, structdata.Success)
+			assert.Equal(test.ExpectedResponse.Data, structdata.Data)
+			assert.Equal(test.ExpectedResponse.Error, structdata.Error)
+
+			// TODO | I don't get the final purpose anyway! Maybe I'm not doing the right test,
+			// TODO | I am testing that the response of a mock request is equal to the input I provided previously.
+			// TODO | Obviously they will match!
+			// TODO | We are not testing if our function returns the expected output for a determined input.
 		})
 	}
 }
