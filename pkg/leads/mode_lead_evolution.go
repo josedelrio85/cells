9cab4aaa63b4c1406b87b860456a20253977d120
Jose del Río <josedelrio85@gmail.com>
2020-07-08 13:28:19 +0200

    changes to match the requirementes of Evolution API

--- pkg/leads/mode_lead_evolution.go
+++ pkg/leads/mode_lead_evolution.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 
@@ -15,68 +16,68 @@ import (
 // Evolution represents data structure needed in SC
 type Evolution struct {
 	Properties     Properties     `json:"propiedades"`
-	AdditionalData AdditionalData `json:"datosAdicionales,omitempty"`
-	Localizators   Localizators   `json:"localizadores,omitempty"`
+	AdditionalData AdditionalData `json:"datosAdicionales"`
+	Localizators   Localizators   `json:"localizadores"`
 }
 
 // Properties bla
 type Properties struct {
-	SubjectID       string `json:"idsujeto,omitempty"`
+	// SubjectID       string `json:"idsujeto"`
 	OriginalID      string `json:"idoriginal"`
-	CampaingID      string `json:"idcampanya"`
-	Name            string `json:"nombre,omitempty"`
-	Surname         string `json:"apellido,omitempty"`
-	Surname2        string `json:"apellido2,omitempty"`
+	CampaingID      int64  `json:"idcampanya"`
+	Name            string `json:"nombre"`
+	Surname         string `json:"apellido"`
+	Surname2        string `json:"apellidO2"`
 	Phone           string `json:"telefono"`
-	Phone2          string `json:"telefono2,omitempty"`
-	PhoneWork       string `json:"telefonoTrabajo,omitempty"`
-	MobilePhone     string `json:"movil,omitempty"`
-	MobilePhone2    string `json:"movil2,omitempty"`
-	Address         string `json:"direccion,omitempty"`
-	PostalCode      string `json:"codigoPostal,omitempty"`
-	Town            string `json:"poblacion,omitempty"`
-	State           string `json:"provincia,omitempty"`
-	Country         string `json:"pais,omitempty"`
-	Fax             string `json:"fax,omitempty"`
-	Email           string `json:"email,omitempty"`
-	Email2          string `json:"emaiL2,omitempty"`
-	BirthDate       string `json:"fechA_NACIMIENTO,omitempty"`
-	SignupDate      string `json:"fechA_ALTA,omitempty"`
-	LanguageID      int64  `json:"iD_IDIOMA,omitempty"`
-	Observations    string `json:"observaciones,omitempty"`
-	LocatableSince  int64  `json:"localizablE_DESDE,omitempty"`
-	LocatableFrom   int64  `json:"localizablE_HASTA,omitempty"`
-	DNI             string `json:"sDNI,omitempty"`
-	FullName        string `json:"sNombre_Completo,omitempty"`
-	Company         string `json:"sEmpresa,omitempty"`
-	Sex             string `json:"cSexo,omitempty"`
-	Text1           string `json:"textO1,omitempty"`
-	Text2           string `json:"textO2,omitempty"`
-	Text3           string `json:"textO3,omitempty"`
-	FavSource       int64  `json:"nCanalPreferencial,omitempty"`
-	Num1            int64  `json:"nuM1,omitempty"`
-	Num2            int64  `json:"nuM2,omitempty"`
-	Num3            int64  `json:"nuM3,omitempty"`
-	SegmentAttibute string `json:"atributo_Segmento,omitempty"`
-	Priority        int64  `json:"prioridad,omitempty"`
-	NextContact     string `json:"tProximo_Contacto,omitempty"`
-	NState          string `json:"nEstado,omitempty"`
-	// Skill           string `json:"atributo_Skill,omitempty"`
-	// NList           string `json:"nLista,omitempty"`
+	Phone2          string `json:"telefono2"`
+	PhoneWork       string `json:"telefonoTrabajo"`
+	MobilePhone     string `json:"movil"`
+	MobilePhone2    string `json:"movil2"`
+	Address         string `json:"direccion"`
+	PostalCode      string `json:"codigO_POSTAL"`
+	Town            string `json:"poblacion"`
+	State           string `json:"provincia"`
+	Country         string `json:"pais"`
+	Fax             string `json:"fax"`
+	Email           string `json:"email"`
+	Email2          string `json:"emaiL2"`
+	BirthDate       string `json:"fechA_NACIMIENTO"`
+	SignupDate      string `json:"fechA_ALTA"`
+	LanguageID      int64  `json:"iD_IDIOMA"`
+	Observations    string `json:"observaciones"`
+	LocatableSince  int64  `json:"localizablE_DESDE"`
+	LocatableFrom   int64  `json:"localizablE_HASTA"`
+	DNI             string `json:"sDNI"`
+	FullName        string `json:"sNombre_Completo"`
+	Company         string `json:"sEmpresa"`
+	Sex             string `json:"cSexo"`
+	Text1           string `json:"textO1"`
+	Text2           string `json:"textO2"`
+	Text3           string `json:"textO3"`
+	FavSource       int64  `json:"nCanalPreferencial"`
+	Num1            int64  `json:"nuM1"`
+	Num2            int64  `json:"nuM2"`
+	Num3            int64  `json:"nuM3"`
+	SegmentAttibute string `json:"atributo_Segmento"`
+	Priority        int64  `json:"prioridad"`
+	NextContact     string `json:"tProximo_Contacto"`
+	NState          string `json:"nEstado"`
+	// Skill           string `json:"atributo_Skill"`
+	// NList           string `json:"nLista"`
 }
 
 // AdditionalData bla
 type AdditionalData struct {
-	AddProp1 string `json:"additionalProp1,omitempty"`
-	AddProp2 string `json:"additionalProp2,omitempty"`
-	AddProp3 string `json:"additionalProp3,omitempty"`
+	AddProp1 string `json:"additionalProp1"`
+	AddProp2 string `json:"additionalProp2"`
+	AddProp3 string `json:"additionalProp3"`
 }
 
 // Localizators bla
 type Localizators struct {
-	AddProp1 int64 `json:"additionalProp1,omitempty"`
-	AddProp2 int64 `json:"additionalProp2,omitempty"`
-	AddProp3 int64 `json:"additionalProp3,omitempty"`
+	AddProp1 int64 `json:"additionalProp1"`
+	AddProp2 int64 `json:"additionalProp2"`
+	AddProp3 int64 `json:"additionalProp3"`
 }
 
 // EvolutionResp represents response from Evolution API
@@ -86,19 +87,27 @@ type EvolutionResp struct {
 
 // LeadToEvolution maps leads values to Evolution struct
 func (lead Lead) LeadToEvolution() Evolution {
+	// TODO this code should be removed when Evolution API
+	// allows to send empty data in this fields
+	f1 := "2020-07-08"
+	f2 := "2020-07-12"
+
 	evolution := Evolution{
 		Properties: Properties{
-			OriginalID: *lead.LeaPhone,
-			CampaingID: lead.SouIDEvolution,
-			Phone:      *lead.LeaPhone,
-			Name:       *lead.LeaName,
-			Email:      *lead.LeaMail,
-			DNI:        *lead.LeaDNI,
+			OriginalID:  *lead.LeaPhone,
+			CampaingID:  lead.SouIDEvolution,
+			Phone:       *lead.LeaPhone,
+			Name:        checkPointerValue(lead.LeaName),
+			Email:       checkPointerValue(lead.LeaMail),
+			DNI:         checkPointerValue(lead.LeaDNI),
+			BirthDate:   f1,
+			SignupDate:  f1,
+			NextContact: f2,
 		},
 		AdditionalData: AdditionalData{
-			AddProp1: *lead.LeaURL,
+			AddProp1: checkPointerValue(lead.LeaURL),
 			AddProp2: *lead.LeaIP,
-			AddProp3: *lead.GaClientID,
+			AddProp3: checkPointerValue(lead.GaClientID),
 		},
 		Localizators: Localizators{
 			AddProp1: int64(lead.ID),
@@ -141,6 +150,7 @@ func (e Evolution) Active(lead Lead) bool {
 	switch lead.SouID {
 	// virgin
 	case 79:
+		log.Printf("souid %d Evolution active", lead.SouID)
 		return true
 	default:
 		return false
@@ -151,6 +161,7 @@ func (e Evolution) Active(lead Lead) bool {
 func (e Evolution) Send(lead Lead) ScResponse {
 	leadevolution := lead.LeadToEvolution()
 	bytevalues, err := json.Marshal(leadevolution)
+	// fmt.Println(string(bytevalues))
 	if err != nil {
 		return ScResponse{
 			Success:    false,
@@ -160,7 +171,6 @@ func (e Evolution) Send(lead Lead) ScResponse {
 		}
 	}
 
-	// TODO create env_var
 	endpoint, ok := os.LookupEnv("EVOLUTION_ENDPOINT")
 	if !ok {
 		err := errors.New("unable to load Lead Evolution URL endpoint")
@@ -178,7 +188,6 @@ func (e Evolution) Send(lead Lead) ScResponse {
 	}
 	client := &http.Client{Transport: transCfg}
 	resp, err := client.Post(endpoint, "application/json", bytes.NewBuffer(bytevalues))
-	// resp, err := http.Post(endpoint, "application/json", bytes.NewBuffer(bytevalues))
 	if err != nil {
 		return ScResponse{
 			Success:    false,
@@ -200,20 +209,26 @@ func (e Evolution) Send(lead Lead) ScResponse {
 		}
 	}
 
+	log.Println(evolutionresp.Success)
+	err = nil
+	status := http.StatusOK
 	if !evolutionresp.Success {
-		return ScResponse{
-			Success:    evolutionresp.Success,
-			StatusCode: http.StatusUnprocessableEntity,
-			ID:         0,
-			Error:      errors.New(fmt.Sprintf("Evolution response %t", evolutionresp.Success)),
-		}
+		err = errors.New(fmt.Sprintf("Evolution response %t", evolutionresp.Success))
+		status = http.StatusUnprocessableEntity
 	}
 
 	return ScResponse{
 		Success:    evolutionresp.Success,
-		StatusCode: http.StatusOK,
+		StatusCode: status,
 		// TODO should be SubjectID field, not implemented by provider
 		ID:    0,
-		Error: nil,
+		Error: err,
 	}
+}
+
+func checkPointerValue(value *string) string {
+	if value != nil {
+		return *value
+	}
+	return ""
 }
